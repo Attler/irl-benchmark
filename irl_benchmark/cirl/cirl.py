b1672bdb2e136f223c1b0a005fc2060324f4451a
NixGD <nixgdill@gmail.com>
2019-05-01 17:28:46 +0200

    Move cirl file

--- irl_benchmark/cirl/cirl.py
+++ irl_benchmark/cirl/cirl.py
@@ -7,6 +7,9 @@ import gym
 import numpy as np
 from irl_benchmark.rl.model.model_wrapper import BaseWorldModelWrapper
 from irl_benchmark.utils.wrapper import unwrap_env
+from irl_benchmark.cirl import cirl_wrapper
+from math import inf
+import tqdm
 
 # Type Alias
 Trajectory = Dict[str, list]
@@ -36,19 +39,35 @@ class ExaustiveSearchCIRL(BaseIRLAlgorithm):
 
         self.no_states = self.model_wrapper.n_states()
         self.no_actions = env.action_space.n
-        self.transitions = self.model_wrapper.get_transition_array()
 
+        self.env = env
 
-    def cirl_trajectories(self, expected_features):
-        trajs = self.generate_trajs()
-        best_traj = max(trajs, key=self.evaluate_traj)
+    def cirl_trajectory(self, expected_features):
 
+        trajs = _cartesian_product( *[np.arange(env.action_space.n) for _ in range(10)])
 
-    def generate_trajs(self) -> Trajectories:
-        self.max_length = 10
-        state = env.reset()
-        for action in range(self.no_actions):
-            pass
+        cirl_env = cirl_wrapper(self.env, expected_features)
+        r = lambda t: self.cirl_reward(cirl_env, t)
 
-    def evaluate_traj(self, traj : Trajectory) -> float:
-        raise NotImplimentedError()
+        return max(trajs, key=r)
+
+    def cirl_reward(cirl_env : gym.Env, traj : np.ndarray) -> int:
+        cirl_env.reset()
+        for a in traj:
+            _ , reward, terminated, _ = cirl_env.step()
+            if terminated:
+                return reward
+
+        # Trajectory didn't terminate :(
+        return - inf
+
+    def _cartesian_product(*arrays):
+        """
+        From https://stackoverflow.com/a/11146645
+        """
+        la = len(arrays)
+        dtype = np.result_type(*arrays)
+        arr = np.empty([len(a) for a in arrays] + [la], dtype=dtype)
+        for i, a in enumerate(np.ix_(*arrays)):
+            arr[...,i] = a
+        return arr.reshape(-1, la)
